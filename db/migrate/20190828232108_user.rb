e9ff1de612c66eb0ab366c8e9af41594d11e30f8
Nia Bert <nbert14@gmail.com>
2019-08-28 23:22:27 +0000

    Automatically backed up by Learn

--- db/migrate/20190828232108_user.rb
+++ db/migrate/20190828232108_user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string :username
       t.string :password
-      t.in
+      t.integer :balance
     end
   end
 end
